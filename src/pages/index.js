946a56a94a3ae02b08b19b7acfc0845491b799cb
phantom05 <monster2jy@gmail.com>
2021-01-15 10:45:28 +0900

    change file structure
    change file structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,19 @@ import { StickyProvider } from "contexts/app/app.provider";
 import theme from "theme";
 import SEO from "components/base/seo";
 import Layout from "components/base/template/layout";
-import Banner from "sections/banner";
-import Services from "sections/services";
-import Jackpot from "sections/jackpot";
-import CallToAction from "sections/call-to-action";
-import Partnership from "sections/partnership";
-import Featured from "sections/featured";
-import Pricing from "sections/pricing";
-import Testimonials from "sections/testimonials";
-import Blogs from "sections/blogs";
-import News from "sections/news";
-import FAQ from "sections/faq";
-import Subscribe from "sections/subscribe";
-import Contact from "sections/contact";
+import Banner from "components/common/sections/banner";
+import Services from "components/common/sections/services";
+import Jackpot from "components/common/sections/jackpot";
+import CallToAction from "components/common/sections/call-to-action";
+import Partnership from "components/common/sections/partnership";
+import Featured from "components/common/sections/featured";
+import Pricing from "components/common/sections/pricing";
+import Testimonials from "components/common/sections/testimonials";
+import Blogs from "components/common/sections/blogs";
+import News from "components/common/sections/news";
+import FAQ from "components/common/sections/faq";
+import Subscribe from "components/common/sections/subscribe";
+import Contact from "components/common/sections/contact";
 
 export default function IndexPage() {
   return (
